010f2b6546ad8128117afbd981e5254d468f8b61
Roman Sapunov <Sapunovrm@bk.ru>
2024-12-05 00:42:49 +0500

    2 Задание

--- ClassesHomework.py
+++ ClassesHomework.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course].append(grade)
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка: Лектор не прикреплен к курсу или студент не записан на курс.'
+
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -26,12 +36,22 @@ class Mentor:
 class Lecturer(Mentor): # Лекторы
     def __init__(self, name, surname):
         super().__init__(name, surname)
+        self.grades = {}
 
 
 class Reviewer(Mentor): # Эксперты, проверяющие домашние задания
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        
+
+    def check_homework(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached:
+            if course in student.grades:
+                student.grades[course].append(grade)
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
@@ -44,3 +64,21 @@ cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
+
+# Создание лекторов и экспертов
+lecturer = Lecturer('Mark', 'Twin')
+lecturer.courses_attached += ['Python']
+
+reviewer = Reviewer('Jane', 'Smith')
+reviewer.courses_attached += ['Python']
+
+# Эксперт проверяет домашние задания
+reviewer.check_homework(best_student, 'Python', 9)
+
+print(best_student.grades)
+
+# Студент выставляет оценку лектору
+best_student.rate_lecturer(lecturer, 'Python', 10)
+best_student.rate_lecturer(lecturer, 'Python', 8)
+
+print(lecturer.grades)
